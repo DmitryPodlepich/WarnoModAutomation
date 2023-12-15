477550c96d27d813b6178a02b373f416347c27ed
dpodlepich <dpodlepich@dev.gotmojo.com>
2023-12-15 15:52:04 +0300

    NDF draft Serealization (string values bug presist)

---
+++ WarnoModeAutomation/DTO/DescriptorUnit.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarnoModeAutomation.DTO
+{
+    public class PropertyToObject 
+    {
+        public object ParentObject { get; set; }
+        public PropertyInfo PropertyInfo { get; set; }
+    }
+
+    public class FileDescriptor 
+    {
+        public readonly List<TEntityDescriptor> EntityDescriptors = [];
+
+        public readonly Dictionary<Guid,string> RawLines = [];
+
+        public readonly Dictionary<Guid, PropertyToObject> Map = [];
+
+        public static readonly Dictionary<string, Type> ModulesDescriptorTypesMap = new()
+        {
+            { "TSupplyModuleDescriptor", typeof(TSupplyModuleDescriptor) }
+        };
+
+        public string Serialize()
+        {
+            var sb = new StringBuilder(RawLines.Count);
+
+            foreach(var rawLine in RawLines)
+            {
+                if (!Map.ContainsKey(rawLine.Key))
+                {
+                    sb.AppendLine(rawLine.Value);
+                    continue;
+                }
+
+                var mapInstantce = Map[rawLine.Key];
+
+                var splitted = rawLine.Value.Split('=');
+
+                var rawValue = splitted[1].Trim();
+
+                var propertyValue = mapInstantce.PropertyInfo.GetValue(mapInstantce.ParentObject);
+
+                var modifiedValue = rawValue.Replace(rawValue, propertyValue.ToString());
+
+                splitted[1] = modifiedValue;
+
+                var modifiedLine = string.Join("", splitted);
+
+                sb.AppendLine(modifiedLine);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public class TEntityDescriptor : Descriptor
+    {
+        public string ClassNameForDebug { get; set; }
+
+        public List<object> ModulesDescriptors { get; set; } = new();
+    }
+
+    public class TSupplyModuleDescriptor : Descriptor, TModulesDescriptor
+    {
+        public float SupplyCapacity { get; set; }
+    }
+
+    public interface TModulesDescriptor{}
+
+    public class Descriptor 
+    {
+        public static readonly Type Type = typeof(TEntityDescriptor);
+
+        public readonly PropertyInfo[] PropertiesInfo;
+
+        public Descriptor()
+        {
+            PropertiesInfo = Type.GetProperties();
+        }
+    }
+}
--- WarnoModeAutomation/Logic/ModManager.cs
+++ WarnoModeAutomation/Logic/ModManager.cs
@@ -1,9 +1,18 @@
-﻿namespace WarnoModeAutomation.Logic
+﻿using Microsoft.Maui.Storage;
+using System.Globalization;
+using System.Text;
+using WarnoModeAutomation.DTO;
+
+namespace WarnoModeAutomation.Logic
 {
     public static class ModManager
     {
         private const string _createNewModBatFileName = "CreateNewMod.bat";
         private const string _generateModBatFileName = "GenerateMod.bat";
+
+        private static readonly string _buildingDescriptorsPath
+            = Path.Combine(Storage.ModeSettings.ModsDirectory, Storage.ModeSettings.ModeName, 
+                "GameData", "Generated", "Gameplay", "Gfx", "BuildingDescriptors.ndf");
 
         public delegate void Outputter(string data);
         public static event Outputter OnOutput;
@@ -63,6 +72,171 @@
             cmdProvier.OnOutput += OnCMDProviderOutput;
 
             return await cmdProvier.PerformCMDCommand(_generateModBatFileName);
+        }
+
+        public static void Run()
+        {
+            if (!File.Exists(_buildingDescriptorsPath))
+            {
+                OnOutput?.Invoke($"{_buildingDescriptorsPath} not found!.");
+                return;
+            }
+
+            var fileDescriptior = new FileDescriptor();
+
+            TEntityDescriptor instance = null;
+            object childInstanse = null;
+
+            bool isReadingEntiryDescriptorClass = false;
+            bool isReadingModulesDescriptorsCollection = false;
+            bool isReadingModuleDescriptor = false;
+
+            //try
+            //{
+            const int bufferSize = 128;
+            using (var fileStream = File.OpenRead(_buildingDescriptorsPath))
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, bufferSize))
+            {
+                string line;
+                var i = 0;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var rawLineKey = Guid.NewGuid();
+
+                    fileDescriptior.RawLines.Add(rawLineKey, line);
+                    i++;
+
+                    var key = string.Empty;
+                    var value = string.Empty;
+                    var lineHasKeyAndValue = line.Contains("=");
+
+                    if (lineHasKeyAndValue)
+                    {
+                        key = line.Split('=')[0].Trim();
+                        value = line.Split("=")[1].Trim();
+                    }
+
+                    if (!isReadingEntiryDescriptorClass && line.Trim().Equals("("))
+                    {
+                        instance = new TEntityDescriptor();
+                        isReadingEntiryDescriptorClass = true;
+                        continue;
+                    }
+
+                    if (isReadingEntiryDescriptorClass && !isReadingModulesDescriptorsCollection && line.Trim().Equals(")"))
+                    {
+                        fileDescriptior.EntityDescriptors.Add(instance);
+
+                        instance = null;
+                        isReadingEntiryDescriptorClass = false;
+                        continue;
+                    }
+
+                    if (isReadingModulesDescriptorsCollection && line.Trim().Equals("("))
+                    {
+                        var previousLine = fileDescriptior.RawLines.ElementAt(i - 2);
+
+                        var moduleDescriptorTypeName = previousLine.Value.Trim();
+
+                        if (FileDescriptor.ModulesDescriptorTypesMap.ContainsKey(moduleDescriptorTypeName))
+                        {
+                            var type = FileDescriptor.ModulesDescriptorTypesMap[moduleDescriptorTypeName];
+                            childInstanse = Activator.CreateInstance(type);
+                        }
+
+                        isReadingModuleDescriptor = true;
+
+                        continue;
+                    }
+
+                    if (isReadingModulesDescriptorsCollection && !isReadingModuleDescriptor && line.Trim().Equals("]"))
+                    {
+                        isReadingModulesDescriptorsCollection = false;
+                        continue;
+                    }
+
+                    if (isReadingModulesDescriptorsCollection && isReadingModuleDescriptor && line.Trim().Equals("),"))
+                    {
+                        if(childInstanse is not null)
+                            instance.ModulesDescriptors.Add(childInstanse);
+
+                        childInstanse = null;
+
+                        isReadingModuleDescriptor = false;
+                        continue;
+                    }
+
+                    if (isReadingModuleDescriptor)
+                    {
+                        if (lineHasKeyAndValue)
+                        {
+                            var applicableProperty = childInstanse?.GetType().GetProperties().SingleOrDefault(p => p.Name == key);
+
+                            if (applicableProperty is null)
+                                continue;
+
+                            if (value.Equals(string.Empty))
+                                continue;
+
+                            fileDescriptior.Map.Add(rawLineKey, new PropertyToObject() { ParentObject = childInstanse, PropertyInfo = applicableProperty });
+
+                            var parsedValue = Convert.ChangeType(value, applicableProperty.PropertyType, CultureInfo.InvariantCulture);
+
+                            applicableProperty.SetValue(childInstanse, parsedValue);
+                        }
+
+                        continue;
+                    }
+
+                    if (isReadingEntiryDescriptorClass)
+                    {
+                        var applicableProperty = instance.PropertiesInfo.SingleOrDefault(p => p.Name == key);
+
+                        if (applicableProperty is not null)
+                        {
+                            if (value.Equals(string.Empty))
+                                continue;
+
+                            //For Collections
+                            if (value.Equals("["))
+                            {
+                                isReadingModulesDescriptorsCollection = true;
+                                continue;
+                            }
+
+                            if (isReadingModulesDescriptorsCollection)
+                                continue;
+
+                            fileDescriptior.Map.Add(rawLineKey, new PropertyToObject() { ParentObject = instance, PropertyInfo = applicableProperty });
+
+                            var parsedValue = Convert.ChangeType(value, applicableProperty.PropertyType);
+                                    
+                            applicableProperty.SetValue(instance, parsedValue);
+
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            foreach (var item in fileDescriptior.EntityDescriptors)
+            {
+                item.ClassNameForDebug = item.ClassNameForDebug + "1";
+            }
+
+            var stringToSave = fileDescriptior.Serialize();
+
+            File.WriteAllText(_buildingDescriptorsPath, stringToSave);
+            //}
+            //catch (Exception ex) 
+            //{
+            //    OnOutput?.Invoke(ex.Message);
+            //}
+        }
+
+        public static void Save()
+        {
+            
         }
 
         private static void OnCMDProviderOutput(string data)
